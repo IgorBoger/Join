4937d05e0366b91b1c3c692417b8df9c06b616ef
Mark Corzilius <marccorzprivate@gmail.com>
2025-04-19 19:21:54 +0200

    testing edit contacts in firebase

--- js/contacts_remote_storage.js
+++ js/contacts_remote_storage.js
@@ -22,6 +22,13 @@ async function postContacts(path='', data={}) {
     return await response.json();
 }
 
+async function deleteContacts(path='') {
+    let response = await fetch(BASE_URL + path + '.json', {
+        method: 'DELETE'
+    });
+    return await response.json();
+}
+
 function sanitizeEmail(email) {
     return email.replace(/[@.]/g, "_");
 }
@@ -62,17 +69,16 @@ async function getNewContactData() {
 async function saveNewContactToDataBase() {
     const {nameValue, emailValue, phoneValue} = await getNewContactData();
     const filled = inputsFilledOut({nameValue, emailValue, phoneValue});
-    const safeKey = sanitizeEmail(contact.email);
+    const safeKey = sanitizeEmail(emailValue);
     const path = 'contacts/' + safeKey;
     if (!filled) return;
 
     const exists = await doesContactExists({emailValue});
     if (exists) {
-        alert("contact already exists");
         return;
+    } else {
+    putContacts(path, {nameValue, emailValue, phoneValue});
     }
-    postContacts(path, {nameValue, emailValue, phoneValue});
-
 }
 
 function inputsFilledOut({nameValue, emailValue, phoneValue}) {
@@ -86,12 +92,58 @@ function inputsFilledOut({nameValue, emailValue, phoneValue}) {
 async function doesContactExists({emailValue}) {
     const response = await fetch(BASE_URL + 'contacts/' + '.json');
     const data = await response.json();
-    const sanitizedEmailValue = sanitizeEmail(emailValue);
 
     for (const key in data) {
-        if (data[key].email === sanitizedEmailValue) {
+        if (data[key].email === emailValue) {
             return true;
         }
     }
     return false;
     }
+
+    async function deleteContactInDataBase(email) {
+        const sanitizedEmail = await sanitizeEmail(email);
+        deleteContacts(`contacts/${sanitizedEmail}`)
+    }
+
+
+    async function changeContactData() {
+        const {nameValue, emailValue, phoneValue} = await getEditedContactData();
+        if (areEditContactFieldsEmpty({nameValue, emailValue, phoneValue})) return;
+        const currentContactKey = await determineCurrentContact();
+        
+        
+        putContacts(`contacts/${currentContactKey}`, {nameValue, emailValue, phoneValue});
+    }
+
+    async function getEditedContactData() {
+        const nameInput = document.getElementById('editName');
+        const emailInput = document.getElementById('editEmail');
+        const phoneInput = document.getElementById('editPhone');
+        const nameValue = nameInput.value;
+        const emailValue = emailInput.value;
+        const phoneValue = phoneInput.value;
+
+        return {nameValue, emailValue, phoneValue};
+    }
+
+    function areEditContactFieldsEmpty({nameValue, emailValue, phoneValue}) {
+        return (nameValue !== '' && emailValue !== '' && phoneValue !== '');
+    }
+
+    async function determineCurrentContact(email) {
+        const sanitizedEmail = sanitizeEmail(email);
+        const response = await fetch(BASE_URL + 'contacts.json');
+        const keys = await response.json();
+        for (const key of Object.keys(keys)) {
+            console.log('Key is ', key);
+            console.log('Email is ', sanitizedEmail);
+            
+            if (sanitizedEmail === key) {
+                return key;
+            }
+        }
+    }
+    //1: Find clicked contact
+    //2: onclick "save" get inputs values
+    //3: replace clicked contact with new contact object
